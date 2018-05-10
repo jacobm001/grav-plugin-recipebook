c88ce8bf430fbad5294a992844eeccb55820bc8a
Jacob Mastel <jacob@mastel.org>
2018-05-10 09:54:22 -0700

    built ability for recipe class to pull recipe data without the one_line_recipes view

--- classes/recipe.php
+++ classes/recipe.php
@@ -1,6 +1,6 @@
 <?php
 	class Recipe implements JsonSerializable {
-		private $id;
+		private $uuid;
 		private $name;
 		private $yields;
 		private $notes;
@@ -8,15 +8,99 @@
 		private $directions;
 		private $tags = array();
 
-		public function __construct($id, $name, $yields, $notes, $directions)
+		public function __construct(&$db, $uuid = null)
 		{
-			$this->id          = $id;
-			$this->name        = $name;
-			$this->yields      = $yields;
-			$this->notes       = $notes;
-			$this->directions  = $directions;
-			$this->ingredients = array();
-			$this->tags        = array();
+			if $uuid == null {
+				$this->id = $this->make_new_uuid();
+				return;
+			}
+			else {
+				$this->uuid = $uuid;
+				$this->populate_recipe();
+			}
+		}
+
+		private function make_new_uuid()
+		{
+			while( True ) {
+				$trial_uuid = uniqid();
+				
+				$stmt = $this->db->prepare('select 1 from recipes where uuid = :trial_uuid;');
+				$stmt->bindParam(':trial_uuid', $trial_uuid, PDO::PARAM_INT);
+				$stmt->execute();
+
+				if( $stmt->fetch() == false )
+					return $trial_uuid;
+			}
+		}
+
+		private function populate_recipe()
+		{
+			$this->populate_recipe_base();
+			$this->populate_recipe_ingredients();
+			$this->populate_recipe_tags();
+		}
+
+		private function populate_recipe_base()
+		{
+			$qry = '''
+				select 
+					user, name, notes, yields, directions
+				from
+					recipes
+				where
+					uuid = :uuid;
+			''';
+
+			$stmt = $this->db->prepare($qry);
+			$stmt->bindParam(':uuid', $this->uuid, PDO::PARAM_INT);
+			$stmt->execute();
+
+			$res = $stmt->fetchOne(PDO::FETCH_ASSOC);
+
+			$this->user       = $res['user'];
+			$this->name       = $res['name'];
+			$this->notes      = $res['notes'];
+			$this->yields     = $res['yields'];
+			$this->directions = $res['directions'];
+		}
+
+		private function populate_recipe_ingredients()
+		{
+			$qry = '''
+				select ingredient
+				from ingredients
+				where uuid = :uuid;
+			''';
+
+			$stmt = $this->db->prepare($qry);
+			$stmt->bindParam(':uuid', $this->uuid);
+			$stmt->execute();
+
+			$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			foreach( $res as $ingr )
+			{
+				$this->ingredients[] = $ingr;
+			}
+		}
+
+		private function populate_recipe_tags()
+		{
+			$qry = '''
+				select tag
+				from tags
+				where uuid = :uuid;
+			''';
+
+			$stmt = $this->db->prepare($qry);
+			$stmt->bindParam(':uuid', $this->uuid);
+			$stmt->execute();
+
+			$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			foreach( $res as $tag )
+			{
+				$this->tags[] = $tag;
+			}
 		}
 
 		public function set_tag($tag_str)
