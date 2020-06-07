d4893c53eec67e4218d6eb2a81dbd7fb686cd82e
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-06-07 16:25:32 -0700

    updated to new db format

--- queries/build_db.sql
+++ queries/build_db.sql
@@ -4,6 +4,7 @@ create table recipes(
   name text,
   notes text,
   yields text,
+  instruction text,
   directions text
 );
 
@@ -14,42 +15,5 @@ create table tags (
   foreign key(uuid) references recipes(uuid)
 );
 
-create table ingredients (
-  uuid text,
-  ingredient text,
-  primary key(uuid, ingredient),
-  foreign key(recipe_id) references recipes(id)
-);
-
 create index indx_tags_01        on tags(uuid);
 create index indx_tags_02        on tags(tag);
-create index indx_ingredients_01 on ingredients(uuid);
-
-CREATE VIEW one_line_recipes as
-  select
-    recipes.uuid
-    , recipes.id
-    , recipes.name
-    , recipes.notes
-    , recipes.yields
-    , recipes.directions
-    , (
-      select
-        group_concat(ingredients.ingredient, '||')
-      from
-        ingredients
-      where
-        ingredients.uuid = recipes.uuid
-    ) as ingredients
-    , (
-      select
-        group_concat(tags.tag, '||')
-      from
-        tags
-      where
-        tags.uuid = recipes.uuid
-    ) as tags
-  from
-    recipes
-  order by
-    lower(recipes.name);
